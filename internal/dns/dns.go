ac0ef71d9f654c8b3cc90933a1ab42c772c113c7
lordwelch <timmy@narnian.us>
2021-05-23 18:54:09 -0700

    Fix null check

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -547,7 +547,7 @@ func (s *Server) handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 		if len(in.Answer) > 1 {
 			if in.Answer[0].Header().Rrtype == dns.TypeCNAME {
 				for _, rr := range in.Answer {
-					if rr.Header().Rrtype == dns.TypeA {
+					if rr != nil && rr.Header() != nil && rr.Header().Rrtype == dns.TypeA {
 						if newRR, err := s.resolveSubname("", dns.Question{strings.ToLower(rr.Header().Name), dns.TypeA, dns.ClassINET}); err == nil {
 							in.Answer[len(in.Answer)-1] = newRR
 						}
